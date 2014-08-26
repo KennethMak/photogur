a0465727eb206661b276ece096a4576be0807c25
Kenneth <kennethmak@Kenneths-MacBook-Pro.local>
2014-08-26 15:47:16 -0400

    id and show method

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,43 +1,72 @@
 class PicturesController < ApplicationController
 
-
-	  def picture0
-	  	  @picture = { 
+	def index
+	  	@pictures = [
+	  	{ 
 			:title  => "The old church on the coast of White sea",
 			:artist => "Sergey Ershov",
 			:url 	=> "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
-		}
-		end
+		},
 
-		def picture1
-		  @picture = {
+		{
 			:title => "Sea Power",
 			:artist => "Stephen Scullion",
 			:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
-		}
-		end
+		},
 
-		def picture2	
-		  @picture = {
+		{
 			:title => "Into the Poppies",
 			:artist => "John Willhelm",
 			:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
-		}
-		end
+		},
 
-		def picture3
-			@picture = {
+		{
 				:title => "Lexus IS F x2",
 				:artist => "Car and Driver",
 				:url => "http://media.caranddriver.com/images/media/235360/2009-lexus-is-f-by-0-60-and-2009-import-tuner-tein-lexus-is-f-photo-235362-s-1280x782.jpg"
-		}
-		end
+		},
 
-		def picture4
-			@picture = {
+		{
 				:title => "E92 BMW M3 Frzn Blk & GTS",
 				:artist => "bimmerblog",
 				:url => "http://www.bmwblog.com/wp-content/uploads/bmw-m3-gts-m3-black-frozen-06.jpg"
-			}
-		end
+		}
+	   ]
+	end
+
+	def show
+		@pictures = [
+		{ 
+			:title  => "The old church on the coast of White sea",
+			:artist => "Sergey Ershov",
+			:url 	=> "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+		},
+
+		{
+			:title => "Sea Power",
+			:artist => "Stephen Scullion",
+			:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+		},
+
+		{
+			:title => "Into the Poppies",
+			:artist => "John Willhelm",
+			:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+		},
+
+		{
+			:title => "Lexus IS F x2",
+			:artist => "Car and Driver",
+			:url => "http://media.caranddriver.com/images/media/235360/2009-lexus-is-f-by-0-60-and-2009-import-tuner-tein-lexus-is-f-photo-235362-s-1280x782.jpg"
+		},
+
+		{
+			:title => "E92 BMW M3 Frzn Blk & GTS",
+			:artist => "bimmerblog",
+			:url => "http://www.bmwblog.com/wp-content/uploads/bmw-m3-gts-m3-black-frozen-06.jpg"
+		}
+		]	
+		@picture = @pictures[params[:id].to_i]
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,15 @@ Rails.application.routes.draw do
 
   Rails.application.routes.draw do
     get 'pictures' => 'pictures#index'
-
-    get 'pictures/0' => 'pictures#picture0'
-    get 'pictures/1' => 'pictures#picture1'
-    get 'pictures/2' => 'pictures#picture2'
-    get 'pictures/3' => 'pictures#picture3'
-    get 'pictures/4' => 'pictures#picture4'
+    get 'pictures/:id' => 'pictures#show', as: "picture"
   end
+
+  #   get 'pictures/0' => 'pictures#picture0'
+  #   get 'pictures/1' => 'pictures#picture1'
+  #   get 'pictures/2' => 'pictures#picture2'
+  #   get 'pictures/3' => 'pictures#picture3'
+  #   get 'pictures/4' => 'pictures#picture4'
+  # end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
