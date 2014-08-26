aeced6ba03c90107d1a00b16be1b6692ead83d97
Kenneth <kennethmak@Kenneths-MacBook-Pro.local>
2014-08-26 15:08:57 -0400

    each pic own URL

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,17 +1,20 @@
 class PicturesController < ApplicationController
-	def index
-	  @pictures = [
-		{ 
+
+
+	  def pictures0
+	  	@picture = { 
 			:title  => "The old church on the coast of White sea",
 			:artist => "Sergey Ershov",
 			:url 	=> "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
-		},
-		{
+		}
+		def picture1
+		@picture = {
 			:title => "Sea Power",
 			:artist => "Stephen Scullion",
-			:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg",
-		},
-		{
+			:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+		}
+
+		@picture = {
 			:title => "Into the Poppies",
 			:artist => "John Willhelm",
 			:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
