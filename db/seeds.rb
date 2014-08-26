0994e6e062684851415314b45b66f8c32eb38735
Kenneth <kennethmak@Kenneths-MacBook-Pro.local>
2014-08-26 17:04:22 -0400

    move pics from controller to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+	Picture.create!(
+			:title  => "The old church on the coast of White sea",
+			:artist => "Sergey Ershov",
+			:url 	=> "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+		)
+
+	Picture.create!(
+
+			:title => "Sea Power",
+			:artist => "Stephen Scullion",
+			:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+		)
+
+	Picture.create!(
+
+			:title => "Into the Poppies",
+			:artist => "John Willhelm",
+			:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+		)
+
+	Picture.create!(
+
+			:title => "Lexus IS F x2",
+			:artist => "Car and Driver",
+			:url => "http://media.caranddriver.com/images/media/235360/2009-lexus-is-f-by-0-60-and-2009-import-tuner-tein-lexus-is-f-photo-235362-s-1280x782.jpg"
+		)
+	Picture.create!(
+
+			:title => "E92 BMW M3 Frzn Blk & GTS",
+			:artist => "bimmerblog",
+			:url => "http://www.bmwblog.com/wp-content/uploads/bmw-m3-gts-m3-black-frozen-06.jpg"
+		)
